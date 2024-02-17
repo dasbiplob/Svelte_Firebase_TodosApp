d8a1ba5d62d826e64848f27f186dcc7e167c4f4c
Biplob Das <biplob90.d@gmail.com>
2024-02-17 20:59:22 +0200

    My 1st todos app

---
+++ src/components/Todos.svelte
@@ -0,0 +1,39 @@
+<script>
+    let todos = [{task:"Get Milk", isComplete: false, createdDate: "2024-02-17"}, 
+    {task:"Get Fruits", isComplete: true, createdDate: "2024-02-8"},
+    {task:"Get Eggs", isComplete: true, createdDate: "2024-02-10"}]
+
+
+    let todo ={
+        task: "",
+        isComplete: "false",
+        createdDate: new Date(),
+    };
+
+    //let count = $state(0);
+
+
+    const addTask = () =>{
+        todos = [...todos,todo];
+    }
+
+    $: console.table(todo);
+
+</script>
+
+<input type= "text" placeholder="Add a task" bind:value={todo.task}/>
+<button on:click={addTask}>AddItem</button>
+<ol>
+    {#each todos as todo}
+    <li class:complete={todo.isComplete}>
+        {todo.task}
+    </li>
+    {/each}
+</ol>
+
+
+<style>
+    .complete{
+        text-decoration: line-through;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Todos from "../components/Todos.svelte";
+
+</script>
+<h1>Welcome to ToDOs App with Svelte + Firebase</h1>
+<Todos/>
